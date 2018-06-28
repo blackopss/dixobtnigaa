bfc5691e871855246f7c8cce87fa08dd2e4a9ee6
blackopss <36933564+blackopss@users.noreply.github.com>
2018-06-28 21:19:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -164,7 +164,19 @@ https://hastebin.com/xumiferaru.coffeescript
     }
 });  
   
-  
+  client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc4")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
   
   
     client.on('message', message => {
